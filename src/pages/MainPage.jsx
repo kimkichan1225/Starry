f2183a444e5fa0631cf45fc6581733f6fbfff11a
kimkichan-1 <kimkichan1225@gmail.com>
2025-12-27 20:47:50 +0900

    work7

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -43,13 +43,13 @@ function MainPage() {
         <div className="fixed right-4 top-[25rem] flex flex-col gap-3 z-20">
           {/* 공유 버튼 */}
           <button className="w-12 h-12 bg-white rounded-full flex items-center justify-center shadow-lg hover:bg-gray-100 transition">
-            <svg className="w-6 h-6 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
+            <svg className="w-7 h-7 text-[#6155F5]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.8} d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
             </svg>
           </button>
           {/* 이미지 캡쳐 버튼 */}
-          <button className="w-12 h-12 bg-white rounded-full flex items-center justify-center shadow-lg hover:bg-gray-100 transition border-2 border-purple-400">
-            <svg className="w-6 h-6 text-purple-500" fill="none" stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24">
+          <button className="w-12 h-12 bg-white rounded-full flex items-center justify-center shadow-lg hover:bg-gray-100 transition">
+            <svg className="w-6 h-6 text-[#6155F5]" fill="none" stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24">
               {/* 왼쪽 상단 모서리 */}
               <path strokeLinecap="round" d="M2 9V4a1 1 0 011-1h4" />
               {/* 오른쪽 상단 모서리 */}
@@ -64,7 +64,7 @@ function MainPage() {
           </button>
           {/* 밤하늘 제작 버튼 */}
           <button className="w-12 h-12 bg-white rounded-full flex items-center justify-center shadow-lg hover:bg-gray-100 transition">
-            <svg className="w-6 h-6 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <svg className="w-6 h-6 text-[#6155F5]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
             </svg>
           </button>
